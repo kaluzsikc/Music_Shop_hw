ae5654f985ff52e2626099973d6dd2394c9a51b7
kaluzsikc <kaluzsikc@gmail.com>
2018-10-19 13:13:44 +0100

    Added abstract class Item

--- src/main/java/items/Item.java
+++ src/main/java/items/Item.java
@@ -1,4 +1,26 @@
 package items;
 
 public abstract class Item {
+
+    private String type;
+    private double buyPrice;
+    private double sellPrice;
+
+    public Item(String type, double buyPrice, double sellPrice){
+        this.type = type;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public double getSellPrice() {
+        return sellPrice;
+    }
 }
